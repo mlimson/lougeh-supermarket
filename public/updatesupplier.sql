37a9855e898e8733a272c91ed520060291341838
Myles Limson <myles.limson@biotechfarms.net>
2023-10-04 13:31:18 +0800

    update SP UPDATE supplier

--- public/updatesupplier.sql
+++ public/updatesupplier.sql
@@ -4,7 +4,9 @@ as
 $$
     BEGIN
         UPDATE Suppliers SET CompanyName=name WHERE SupplierCode = supCode AND CompanyName <> name;
-        UPDATE SupplierAddress SET SStreet=street, SCity=city, SCountry=country WHERE SupplierCode = supCode AND SStreet<>street OR SCity=city OR SCountry<>country;
+        UPDATE SupplierAddress SET SStreet=street WHERE SupplierCode = supCode AND SStreet<>street;
+        UPDATE SupplierAddress SET SCity=city WHERE SupplierCode = supCode AND SCity<>city;
+        UPDATE SupplierAddress SET SCountry=country WHERE SupplierCode = supCode AND SCountry<>country;
         addressId = (SELECT SAddressId FROM SupplierAddress WHERE SupplierCode=supCode);
         UPDATE SupplierContact SET ContactNumber=contact WHERE SAddressId=addressId AND ContactNumber<>contact;
         COMMIT;
