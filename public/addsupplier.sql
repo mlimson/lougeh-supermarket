1e855cfa782eef6e5867033899e40dc5283e2ab7
Myles Limson <myles.limson@biotechfarms.net>
2023-10-04 09:02:47 +0800

    added default value for parameters

--- public/addsupplier.sql
+++ public/addsupplier.sql
@@ -17,3 +17,22 @@ $$;
 
 alter procedure addsupplier(inout integer, varchar, inout integer, varchar, varchar, varchar, varchar) owner to postgres;
 
+create procedure addsupplier(IN name character varying, IN street character varying, IN city character varying, IN country character varying, IN contact character varying, INOUT supcode integer DEFAULT 0, INOUT addressid integer DEFAULT 0)
+    language plpgsql
+as
+$$
+    BEGIN
+        INSERT INTO Suppliers (CompanyName, TimeStamp) VALUES (name, current_timestamp)
+        RETURNING (SELECT currval('suppliers_suppliercode_seq')) INTO supCode;
+
+        INSERT INTO SupplierAddress (SupplierCode, SStreet, SCity, SCountry) VALUES (supCode, street, city, country)
+        RETURNING (SELECT currval('supplieraddress_saddressid_seq')) INTO addressId;
+
+        INSERT INTO SupplierContact (SAddressId, ContactNumber)VALUES (addressId, contact);
+
+        COMMIT;
+    END
+$$;
+
+alter procedure addsupplier(varchar, varchar, varchar, varchar, varchar, inout integer, inout integer) owner to postgres;
+
