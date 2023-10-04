65f152cedfe464f38fc89fd52050f0fc7e65699d
Myles Limson <myles.limson@biotechfarms.net>
2023-10-04 10:35:28 +0800

    update SP CREATE

--- public/addcustomer.sql
+++ public/addcustomer.sql
@@ -1,22 +1,3 @@
-create procedure addcustomer(INOUT cuscode integer, IN fname character varying, IN lname character varying, INOUT addressid integer, IN street character varying, IN city character varying, IN country character varying, IN contact character varying)
-    language plpgsql
-as
-$$
-    BEGIN
-        INSERT INTO Customers (FirstName, lastname, TimeStamp) VALUES (fname, lname, current_timestamp)
-        RETURNING (SELECT currval('customers_customercode_seq')) INTO cusCode;
-
-        INSERT INTO CustomerAddress (CustomerCode, CStreet, CCity, CCountry) VALUES (cusCode, street, city, country)
-        RETURNING (SELECT currval('customeraddress_caddressid_seq')) INTO addressId;
-
-        INSERT INTO CustomerContact (CAddressId, CContactNumber)VALUES (addressId, contact);
-
-        COMMIT;
-    END
-$$;
-
-alter procedure addcustomer(inout integer, varchar, varchar, inout integer, varchar, varchar, varchar, varchar) owner to postgres;
-
 create procedure addcustomer(IN fname character varying, IN lname character varying, IN street character varying, IN city character varying, IN country character varying, IN contact character varying, INOUT addressid integer DEFAULT 0, INOUT cuscode integer DEFAULT 0)
     language plpgsql
 as
