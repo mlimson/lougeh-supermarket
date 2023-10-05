3bb24efbc5b1e631696a3de8b14e5c99cb866531
Myles Limson <myles.limson@biotechfarms.net>
2023-10-05 11:05:25 +0800

    update additem SP

--- public/additem.sql
+++ public/additem.sql
@@ -1,4 +1,4 @@
-create procedure additem(IN itmbarcode character, IN itmdescription character varying, IN itmuom integer, IN itmprice numeric, INOUT itmcode integer DEFAULT 0)
+create procedure additem(IN itmbarcode character, IN itmdescription character varying, IN itmuom integer, IN itmprice numeric, IN itmuomid integer DEFAULT 0, INOUT itmcode integer DEFAULT 0)
     language plpgsql
 as
 $$
@@ -6,10 +6,13 @@ $$
         INSERT INTO Item (itembarcode, itemdescription, itemprice) VALUES (itmBarcode, itmDescription, itmPrice)
         RETURNING (SELECT currval('item_itemcode_seq')) INTO itmCode;
 
-        INSERT INTO itemuom (itemcode, uomid) VALUES (itmCode, itmUom);
+        INSERT INTO itemuom (itemcode, uomid) VALUES (itmCode, itmUom)
+        RETURNING (SELECT currval('itemuom_uniqueid_seq')) INTO itmUomId;
+
+        INSERT INTO inventory (itemcode, itemuomid, itemstock) VALUES (itmCode, itmUomId, 0);
         COMMIT;
     END
 $$;
 
-alter procedure additem(char, varchar, integer, numeric, inout integer) owner to postgres;
+alter procedure additem(char, varchar, integer, numeric, integer, inout integer) owner to postgres;
 
