bdc1a2381805888732b37972f994cf4a1f4a2a80
Myles Limson <myles.limson@biotechfarms.net>
2023-10-05 16:53:29 +0800

    SP delete sales transaction

---
+++ public/deletesales.sql
@@ -0,0 +1,21 @@
+create procedure deletesales(INOUT stransid integer, IN itmcode integer DEFAULT 0, IN qty integer DEFAULT 0)
+    language plpgsql
+as
+$$
+    BEGIN
+        IF EXISTS (SELECT stransactionid FROM salestransaction WHERE stransactionid = sTransId)
+        THEN
+            qty = (SELECT sqty FROM salesline WHERE stransactionid = sTransId);
+            itmCode = (SELECT itemcode FROM salesline WHERE stransactionid = sTransId);
+            UPDATE inventory SET itemstock = itemstock + qty WHERE itemcode = itmCode;
+            DELETE FROM salesline WHERE stransactionid = sTransId;
+            DELETE FROM salestransaction WHERE stransactionid = sTransId;
+        ELSE
+            RAISE NOTICE 'Transaction not found.';
+        END IF;
+        COMMIT;
+    END
+$$;
+
+alter procedure deletesales(inout integer, integer, integer) owner to postgres;
+
